ee12a5ae243f25ee2bc70a84a609fe4b13e0b852
Kasztl Richard <richikasztl05@gmail.com>
2025-04-15 09:35:53 +0200

    Database seeding

--- src/main/java/com/esen/bookstore/model/Book.java
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -6,9 +6,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
-@Data
 @Entity
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -16,4 +16,55 @@ public class Book {
     private String author;
     private String publisher;
     private Double price;
+
+    public Book() {
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Book(long id, String title, String author, String publisher, Double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.publisher = publisher;
+        this.price = price;
+    }
 }
--- src/main/java/com/esen/bookstore/model/Bookstore.java
+++ src/main/java/com/esen/bookstore/model/Bookstore.java
@@ -1,9 +1,8 @@
 package com.esen.bookstore.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import java.util.Map;
 
-@Data
 @Entity
 public class Bookstore {
     @Id
@@ -15,4 +14,57 @@ public class Bookstore {
     private Double priceModifier;
     private Double moneyInCashRegister;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    private Map<Book, Integer> inventory;
+
+    public Bookstore(long id, String location, Double priceModifier, Double moneyInCashRegister, Map<Book, Integer> inventory) {
+        this.id = id;
+        this.location = location;
+        this.priceModifier = priceModifier;
+        this.moneyInCashRegister = moneyInCashRegister;
+        this.inventory = inventory;
+    }
+
+    public Bookstore() {
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setPriceModifier(Double priceModifier) {
+        this.priceModifier = priceModifier;
+    }
+
+    public void setMoneyInCashRegister(Double moneyInCashRegister) {
+        this.moneyInCashRegister = moneyInCashRegister;
+    }
+
+    public void setInventory(Map<Book, Integer> inventory) {
+        this.inventory = inventory;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Double getPriceModifier() {
+        return priceModifier;
+    }
+
+    public Double getMoneyInCashRegister() {
+        return moneyInCashRegister;
+    }
+
+    public Map<Book, Integer> getInventory() {
+        return inventory;
+    }
 }
