c0b714d1b020017db2bf0e5f1f0429dfbe75a905
Kasztl Richard <richikasztl05@gmail.com>
2025-04-22 09:37:22 +0200

    Start implementing shell commands

--- src/main/java/com/esen/bookstore/model/Book.java
+++ src/main/java/com/esen/bookstore/model/Book.java
@@ -4,11 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Book {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -16,55 +22,4 @@ public class Book {
     private String author;
     private String publisher;
     private Double price;
-
-    public Book() {
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public Book(long id, String title, String author, String publisher, Double price) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.publisher = publisher;
-        this.price = price;
-    }
 }
--- src/main/java/com/esen/bookstore/model/Bookstore.java
+++ src/main/java/com/esen/bookstore/model/Bookstore.java
@@ -1,9 +1,17 @@
 package com.esen.bookstore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Bookstore {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,56 +23,6 @@ public class Bookstore {
     private Double moneyInCashRegister;
 
     @ElementCollection(fetch = FetchType.EAGER)
+    @JsonIgnore
     private Map<Book, Integer> inventory;
-
-    public Bookstore(long id, String location, Double priceModifier, Double moneyInCashRegister, Map<Book, Integer> inventory) {
-        this.id = id;
-        this.location = location;
-        this.priceModifier = priceModifier;
-        this.moneyInCashRegister = moneyInCashRegister;
-        this.inventory = inventory;
-    }
-
-    public Bookstore() {
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public void setPriceModifier(Double priceModifier) {
-        this.priceModifier = priceModifier;
-    }
-
-    public void setMoneyInCashRegister(Double moneyInCashRegister) {
-        this.moneyInCashRegister = moneyInCashRegister;
-    }
-
-    public void setInventory(Map<Book, Integer> inventory) {
-        this.inventory = inventory;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public Double getPriceModifier() {
-        return priceModifier;
-    }
-
-    public Double getMoneyInCashRegister() {
-        return moneyInCashRegister;
-    }
-
-    public Map<Book, Integer> getInventory() {
-        return inventory;
-    }
 }
