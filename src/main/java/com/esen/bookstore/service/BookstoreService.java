5a11d64c600c93fc6b683b72d807908c8080a3b6
Kasztl Richard <richikasztl05@gmail.com>
2025-04-29 09:26:11 +0200

    Start implementing CRUD for Book and Bookstore

---
+++ src/main/java/com/esen/bookstore/service/BookstoreService.java
@@ -0,0 +1,35 @@
+package com.esen.bookstore.service;
+
+import com.esen.bookstore.model.Book;
+import com.esen.bookstore.model.Bookstore;
+import com.esen.bookstore.repository.BookstoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookstoreService {
+
+    private final BookstoreRepository bookstoreRepository;
+
+    public void removeBookFromInventories(Book book){
+        bookstoreRepository.findAll()
+                .forEach(bookstore -> {
+                    bookstore.getInventory()
+                            .remove(book);
+                    bookstoreRepository.save(bookstore);
+                });
+    }
+
+    public List<Bookstore> findAll(){
+        return bookstoreRepository.findAll();
+    }
+
+    public void deleteBookstore(Long id){
+        var bookstore = bookstoreRepository.getReferenceById(id);
+        bookstoreRepository.delete(bookstore);
+    }
+
+}
--- src/main/java/com/esen/bookstore/shell/BookHandler.java
+++ src/main/java/com/esen/bookstore/shell/BookHandler.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
 
 import java.util.stream.Collectors;
 
@@ -17,7 +18,8 @@ public class BookHandler {
 
     private final BookService bookService;
 
-     @ShellMethod(value = "Creates a book", key = "create book")
+
+    @ShellMethod(value = "Creates a book", key = "create book")
     public void createBook(String title, String author,
                            String publisher, Double price){
         bookService.save(Book.builder()
@@ -29,12 +31,31 @@ public class BookHandler {
         );
     }
 
+
     @ShellMethod(value = "Lists all books", key = "list books")
     public String listBooks(){
         return bookService.findAll()
                 .stream()
                 .map(Book::toString)
                 .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+
+    @ShellMethod(value = "Deletes a book by ID", key = "delete book")
+    public void deleteBook(Long id){
+         bookService.delete(id);
+    }
+
+
+    @ShellMethod(value = "Updates a book", key = "update book")
+    public String updateBook(
+                 @ShellOption(defaultValue = ShellOption.NULL) Long id,
+                 @ShellOption(defaultValue = ShellOption.NULL) String title,
+                 @ShellOption(defaultValue = ShellOption.NULL) String author,
+                 @ShellOption(defaultValue = ShellOption.NULL) String publisher,
+                 @ShellOption(defaultValue = ShellOption.NULL) Double price
+    ){
+         return bookService.update(id, title, author, publisher, price).toString();
     }
 
 }
